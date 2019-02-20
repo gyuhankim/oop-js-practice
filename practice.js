e6d22e98669bba0be378e46113e47ca025f56bd7
Joseph Kim <gyuhankim@gmail.com>
2019-02-20 15:11:00 -0500

    created chracter class

--- practice.js
+++ practice.js
@@ -1,6 +1,30 @@
-// Practice
-function hi() {
-    console.log('Hi');
-}
+class Player {
+    constructor(name, hp, stats, money) {
+        this._name = name;
+        this._hp = hp;
+        this._stats = stats;
+        this._money = money;
+    }
 
-hi();
+    get name() { // makes this read-only
+        return this._name;
+    }
+
+    get hp() {
+        return this._hp;
+    }
+
+    get stats() {
+        return this._stats;
+    }
+
+    takeDamage(damage) {
+        console.log(`${this.name} took ${damage} damage. Shrekt...`);
+        return this._hp -= damage;
+    }
+
+    changeName(name) {
+        return this._name = name;
+    }
+
+}
