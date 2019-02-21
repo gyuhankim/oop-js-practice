bacf76f183447aaa3b814dcfacbd228f260b0abf
Joseph Kim <gyuhankim@gmail.com>
2019-02-21 16:43:40 -0500

    exercise 1 complete

--- es6-exercises.js
+++ es6-exercises.js
@@ -1,1 +1,17 @@
 // http://marijnhaverbeke.nl/talks/es6_falsyvalues2015/exercises/#Point
+
+// 1 
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(targetPoint) {
+    return new Point(this.x + targetPoint.x, this.y + targetPoint.y);
+  }
+}
+const coord1 = new Point(1, 2);
+const coord2 = new Point(2, 1);
+console.log(coord1.plus(coord2));
+
+// 2
